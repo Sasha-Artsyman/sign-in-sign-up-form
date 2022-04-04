08fd8a6cf8492c16e428e20108e229b953f9cd3a
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-04-04 19:34:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-// начальные данные
 List<Registered> users = new List<Registered>
 {
     new() { Id = Guid.NewGuid().ToString(), Name = "Admin", Password = "12345678" },
@@ -15,29 +14,18 @@ app.MapGet("/api/users", () => users);
 
 app.MapGet("/api/users/{id}", (string id) =>
 {
-    // получаем по id
     Registered? user = users.FirstOrDefault(u => u.Id == id);
-    // если не найден, отправляем статусный код и сообщение об ошибке
     if (user == null) return Results.NotFound(new { message = "User not found!" });
 
-    // если найден, отправляем его
     return Results.Json(user);
 });
 
-// регистрация
 app.MapPost("/api/users", (Registered user) => {
 
-    // устанавливаем id для нового
     user.Id = Guid.NewGuid().ToString();
-    // добавляем в список
     users.Add(user);
     return user;
 });
-
-
-
-
-
 
 app.Run();
 
